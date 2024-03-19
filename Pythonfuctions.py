0b343aca928680d5fa4bb7453d7af2bde521507d
Wanjiku Mwaura <133624893+MwauraGrace@users.noreply.github.com>
2024-03-19 23:00:32 +0300

    day9

---
+++ Pythonfuctions.py
@@ -0,0 +1,16 @@
+#Defining and calling functions
+name = input("What is your name?")   
+def myfunction():
+    print("Hello")
+    print(name)
+
+myfunction()
+
+
+#Hurdles lop challenge
+def turn_right():
+    print("Next step")
+def jump():
+    print("Final")
+for step in range(6):
+    jump()
